b4b3a55e91d376f54456327c03a9fba7b8bb81ea
MoneshB7 <moneshb7@gmail.com>
2019-05-18 16:48:17 +0530

    Question Updated

--- TriangleValidation.py
+++ TriangleValidation.py
@@ -1,3 +1,6 @@
+"""Write a python function to check whether three given numbers can form the sides of a triangle. 
+Hint: Three numbers can be the sides of a triangle if none of the numbers are greater than or equal to the sum of the other two numbers"""
+
 def form_triangle(num1,num2,num3):
     if (num1 + num2 <= num3) or (num1 + num3 <= num2) or (num2 + num3 <= num1):
         failure="Triangle can't be formed"
